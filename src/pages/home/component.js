fdf63c57f311a2244245a0303f55e336f943bacc
“mluena” <maluenarod@gmail.com>
2020-02-05 13:29:10 +0100

    reset styles

--- src/pages/home/component.js
+++ src/pages/home/component.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Link from 'redux-first-router-link';
-import MapContainer from 'components/map/map-container';
 
 import './styles.scss';
 
 const Home = () => (<div className="c-home">
   <Link to='/'>Home</Link>
-  <MapContainer />
   <Link to='/map'>Map</Link>
 </div>);
 
