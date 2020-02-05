1448f8921b1a4be0f782af2c89b3807e876bd70c
“mluena” <maluenarod@gmail.com>
2020-02-05 13:21:16 +0100

    modal, btn component added

---
+++ src/components/button/component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classnames from 'classnames';
+import './styles.scss';
+
+export default (props) => {
+  const { children,
+    isDisabled,
+    className,
+    ...domProps } = props;
+
+  return (
+    <button
+      type="button"
+      className={classnames('c-button', {
+        ['disabled']: isDisabled,
+        [className]: className
+      })}
+      {...domProps}
+    >
+      {children}
+    </button>
+  );
+};
