654e0a5a94505e369c495e0cce59c83a9172bba7
“mluena” <maluenarod@gmail.com>
2020-02-12 13:39:23 +0100

    hero component styles

--- src/components/tabs-menu/component.js
+++ src/components/tabs-menu/component.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 import './styles.scss';
 
 
-const TabsMenu = ({ tabs, setActiveTab }) => {
+const TabsMenu = ({ current, tabs, setActiveTab }) => {
 
   const setTab = (currentTab) => setActiveTab(currentTab);
 
   return <ul className="c-menu-items">
     {tabs.map(tab =>
       <li
-        className="menu-item-header"
+        className={classnames('menu-item-header',
+          { 'active': current === tab } )}
         tabIndex="0"
         role="button"
         onClick={() => setTab(tab)}
