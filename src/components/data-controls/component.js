ccd62d5cc6b5792541bd91762e206b40ebe0ee3d
“mluena” <maluenarod@gmail.com>
2020-02-20 09:13:50 +0100

    styles modifiers for btn

--- src/components/data-controls/component.js
+++ src/components/data-controls/component.js
@@ -9,14 +9,14 @@ const DataControls = ({ }) => {
   return (
     <div className="c-data-controls">
       <div>
-        <Button className="disabled">
+        <Button className="_download">
           {/* <Icon name="download"/> */}
           Download results
           </Button>
       </div>
       <div className="data-filters">
         <p>Content configuration:</p>
-        <Button className="collapse small" />
+        <Button className="_collapse _small" />
       </div>
 
     </div>
