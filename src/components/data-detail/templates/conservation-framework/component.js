e3665ae40952cdce40c29124ab27d6a046fbae9e
“mluena” <maluenarod@gmail.com>
2020-03-17 08:15:01 +0100

    extra code removed

--- src/components/data-detail/templates/conservation-framework/component.js
+++ src/components/data-detail/templates/conservation-framework/component.js
@@ -1,41 +1,22 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import Button from 'components/button';
 
 import info from './constants';
 import './styles.scss';
 
-const ConservationFramework = () => {
-
-  const [isCollapsed, toggleCollapse] = useState(true);
-
-  const handleClick = () =>{
-    toggleCollapse(!isCollapsed)
-  }
-
-  return (
-    <div className="c-conservation-framework">
-      <div className="header">
-        <h2>{info.title}</h2>
-        <Button
-          onClick={handleClick}
-          className={classnames(
-            { ['-secondary']: isCollapsed,
-              ['-collapse']: !isCollapsed
-            }
-          )}>
-          {isCollapsed ? 'Expand': 'Collapse'}
-        </Button>
-      </div>
-      <span>{info.title}</span>
-      <ul className={classnames({ ['collapse']: !isCollapsed })}>
-        {info.data.map(data =>
-          <li>{data}</li>)}
-      </ul>
+const ConservationFramework = () => (
+  <div className="c-conservation-framework">
+    <div className="header">
+      <h2>{info.title}</h2>
     </div>
-  )
-}
+    <span>{info.title}</span>
+    <ul>
+      {info.data.map(data =>
+        <li>{data}</li>)}
+    </ul>
+  </div>
+)
 
 ConservationFramework.propTypes = {
   info: PropTypes.shape({
