14779bec1b8aa81314c64108f4bbaad3bb59c7da
“mluena” <maluenarod@gmail.com>
2020-03-17 08:05:42 +0100

    explore detail, first components

---
+++ src/components/data-detail/templates/notes/constants.js
@@ -0,0 +1,10 @@
+const info = {
+  title: 'Notes',
+  data:
+    [
+      'S5342 - 413: 650,000 counted in South Asia in 2003-2004.',
+      'S8054 - Li in litt. 2005: 650,000 counted in South Asia in 2003-2004.'
+    ]
+}
+
+export default info;
