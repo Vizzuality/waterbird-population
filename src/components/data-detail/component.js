bafc244f274eafdd9dc755661fd8295d37001fd6
“mluena” <maluenarod@gmail.com>
2020-03-23 11:15:20 +0100

    remove duplicated component filters

--- src/components/data-detail/component.js
+++ src/components/data-detail/component.js
@@ -5,7 +5,6 @@ import PopulationTrend from './templates/population-trend';
 import ConservationFramework from './templates/conservation-framework';
 import References from './templates/references';
 import Notes from './templates/notes';
-import Filters from 'components/filters';
 
 import './styles.scss';
 
@@ -13,7 +12,6 @@ import './styles.scss';
 const DataDetail = ({ info }) =>
     <div className="c-data-detail">
       <PopulationTrend />
-      <Filters />
       <ConservationFramework />
       <References />
       <Notes />
