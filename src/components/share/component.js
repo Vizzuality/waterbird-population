e75c477699bbc76f511183cbec06250d5cf0e794
“mluena” <maluenarod@gmail.com>
2020-05-05 17:55:19 +0200

    share networks

--- src/components/share/component.js
+++ src/components/share/component.js
@@ -1,26 +1,33 @@
+import './styles.scss';
 import React, { useState, useRef } from 'react';
 
 import Modal from 'components/modal';
 import Button from 'components/button';
-
-import './styles.scss';
+import FacebookIcon from 'components/map/map-container/icons/share.svg';
+import TwitterIcon from 'components/map/map-container/icons/share.svg';
+import LinkedynIcon from 'components/map/map-container/icons/share.svg';
+import EmailIcon from 'components/map/map-container/icons/share.svg';
 
 const ShareControl = () => {
-  const inputEl = useRef();
+  const inputElUrl = useRef();
+  const inputElEmbed = useRef();
   const url = window.location.href;
-  const [{ isCopied, isOpen }, setState] = useState(
-    { isCopied: false, isOpen: false }
-    );
 
-  const handleClick = () => {
-    const { current } = inputEl;
-    current.select()
+  const [{ isCopied, isOpen }, setState] = useState({
+    isUrlCopied: false,
+    isEmbedCopied: false,
+    isOpen: false,
+  });
+
+  const handleClick = (param) => {
+    const { current } = param === 'Url' ? inputElUrl : inputElEmbed;
+    current.select();
 
     try {
       document.execCommand('copy');
-      setState({ isCopied: true })
+      setState({ [`is${param}Copied`]: true });
       setTimeout(() => {
-        setState({ isCopied: false })
+        setState({ [`is${param}Copied`]: false });
         current.blur();
       }, 3000);
     } catch (err) {
@@ -28,40 +35,89 @@ const ShareControl = () => {
     }
   };
 
-  const handleModal = () => {
-    setState({ isOpen: !isOpen })
-  }
-
+  const toggleModal = () => {
+    setState({ isOpen: !isOpen });
+  };
 
   return (
-    <div class="mapboxgl-ctrl mapboxgl-ctrl-group  map-share">
-      <button className="mapboxgl-ctrl-icon" onClick={handleModal}>
+    <div className="mapboxgl-ctrl mapboxgl-ctrl-group  map-share">
+      <button className="mapboxgl-ctrl-icon" onClick={toggleModal}>
         <Modal
+          title="Share"
           isOpen={isOpen}
-          onRequestClose={() => setState({ isOpen: false })}
+          onRequestClose={() => toggleModal(false)}
+          actionsComponent={() => (
+            <div className="c-filters-action-buttons">
+              <Button className="-border-color-1" onClick={toggleModal}>
+                Close
+            </Button>
+            </div>
+          )}
         >
           <div className="c-share">
-            <div className="share-content">
-              <h2>Copy link:</h2>
-              <div className="share-controls">
+            <div className="form-fields">
+              <label>Public url to share</label>
+              <div className="share-content">
+                <input ref={inputElUrl} value={url} readOnly />
+
+                <div className="share-controls">
+                  <a
+                    href={`http://www.facebook.com/sharer/sharer.php?u=${url}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {'Facebook' || <img alt="share-facebook" src={FacebookIcon} />}
+                  </a>
+                  <a
+                    href={`https://twitter.com/share?url=${url}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {'Twitter' || <img alt="share-twitter" src={TwitterIcon} />}
+                  </a>
+
+                  <a
+                    href={`mailto:?subject=Shared from Waterbird Estimates Population&body= I thought you'd be interested in some data: ${url}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {'Email' || <img alt="share-email" src={EmailIcon} />}
+                  </a>
+
+                  <a
+                    href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {'Linkedyn' || <img alt="share-linkedin" src={LinkedynIcon} />}
+                  </a>
+
+                  <Button className="-background -primary -medium" onClick={() => handleClick('Url')}>
+                    {isCopied ? 'Copied' : 'Copy'}
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="form-fields">
+              <label>Code to embed</label>
+              <div className="share-content">
                 <input
-                  ref={inputEl}
-                  value={url}
+                  ref={inputElEmbed}
+                  value={`<iframe src="${url}" width="100%" height="500px" frameBorder="0" />`}
                   readOnly
                 />
-                <Button
-                  className="-background -primary -medium"
-                  onClick={handleClick}
-                >
-                  {isCopied ? 'Copied' : 'Copy'}
-                </Button>
+                <div className="share-controls">
+                  <Button className="-background -primary -medium" onClick={() => handleClick('Embed')}>
+                    {isCopied ? 'Copied' : 'Copy'}
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </Modal>
       </button>
     </div>
-  )
+  );
 };
 
 export default ShareControl;
