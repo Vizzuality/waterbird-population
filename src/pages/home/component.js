754565dccdb6bde307ee660dc6b1b3661451c446
“mluena” <maluenarod@gmail.com>
2020-05-11 18:40:52 +0200

    section 2

--- src/pages/home/component.js
+++ src/pages/home/component.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Section1 from 'components/home/section1';
+import Section2 from 'components/home/section2';
 
 import './styles.scss';
 
 const Home = () => (<div className="l-home">
   <Section1 />
+  <Section2 />
 </div>);
 
 export default Home;
