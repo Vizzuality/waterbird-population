b4b05e7fb9b0c7dcde5c924006ba6f3cc4ea1b64
“mluena” <maluenarod@gmail.com>
2020-05-12 10:16:21 +0200

    section 2

--- src/components/home/section2/component.js
+++ src/components/home/section2/component.js
@@ -9,29 +9,31 @@ const Section2 = () => {
   const length = Text.length;
   return (
     <section className="c-section2">
-      <div className="row">
-        <div className="col-md-3 col-sm-12">
-          <img src={Image} alt="Wetlands International" />
-        </div>
-        <div className="col-md-7 col-sm-12">
-          <div className="wrapper">
-          <div className="row">
-            <div className="col-sm-12">
-              <h2>What can I use this tool for?</h2>
-            </div>
+      <div className="wrapper">
+        <div className="row">
+          <div className="col-md-5 col-sm-12">
+            <img src={Image} alt="Wetlands International" />
           </div>
-          <div className="row">
-            <ul>
-              {Text.map(({ id, description }) => (
-                <div className={`col-sm-${
-                  id === length && length % 2 ? 12 : 6}`}>
-                  <li key={id}>
-                    <span>0{id}</span><p>{description}</p>
-                  </li>
+          <div className="col-md-7 col-sm-12">
+            <div className="wrapper">
+              <div className="row">
+                <div className="col-sm-12">
+                  <h2>What can I use this tool for?</h2>
                 </div>
-              ))}
-            </ul>
-          </div>
+              </div>
+              <div className="row">
+                <ul>
+                  {Text.map(({ id, description }) => (
+                    <div className={`col-sm-${
+                      id === length && length % 2 ? 12 : 6}`}>
+                      <li key={id}>
+                        <span>0{id}</span><p>{description}</p>
+                      </li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
