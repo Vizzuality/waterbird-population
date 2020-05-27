119bff0b830ba749b709651ca46ffd040880ab7d
“mluena” <maluenarod@gmail.com>
2020-05-27 15:22:14 +0200

    tabs FAQ fixed

--- src/components/tabs-menu/component.js
+++ src/components/tabs-menu/component.js
@@ -7,8 +7,10 @@ import Link from 'redux-first-router-link';
 import './styles.scss';
 
 const TabsMenu = ({ current, tabs, page }) =>
-  <div className="c-menu-items">
-    {tabs.map(tab =>
+  <div className={classnames('c-menu-items', {
+    '-tabs': tabs
+  })}>
+    {tabs && tabs.map(tab =>
       <Link
         className={classnames('menu-item', { 'active': tab.id === current.id })}
         to={ { type: page,  payload : { id:`${tab.id}` }}}>
