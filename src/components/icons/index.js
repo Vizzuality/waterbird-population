a55cf64d8ff619093eef41d428a0a7ce7b6574db
“mluena” <maluenarod@gmail.com>
2020-05-27 15:16:01 +0200

    old icons back

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -3,69 +3,81 @@ import React from 'react';
 const Icons = () => (
   <svg
     aria-hidden="true"
-    style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}
+    style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden'}}
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xlink="http://www.w3.org/1999/xlink"
   >
     <defs>
       <symbol id="icon-feedback_icon" viewBox="0 0 34 32">
-        <path strokeLinejoin="miter" strokeLinecap="butt" stroke-miterlimit="4" stroke-width="4" d="M3.429 4.364v23.636l9.333-7.091h16.333c1.288 0 2.333-1.059 2.333-2.364v-14.182c0-1.305-1.045-2.364-2.333-2.364h-23.333c-1.288 0-2.333 1.059-2.333 2.364z"></path>
+        <title>feedback_icon</title>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="4" d="M3.429 4.364v23.636l9.333-7.091h16.333c1.288 0 2.333-1.059 2.333-2.364v-14.182c0-1.305-1.045-2.364-2.333-2.364h-23.333c-1.288 0-2.333 1.059-2.333 2.364z"></path>
       </symbol>
       <symbol id="icon-flecha_01" viewBox="0 0 32 32">
-        <path strokeLinejoin="miter" strokeLinecap="butt" stroke-miterlimit="4" stroke-width="1.6842" d="M31.158 16c0 8.371-6.786 15.158-15.158 15.158s-15.158-6.786-15.158-15.158c0-8.371 6.786-15.158 15.158-15.158s15.158 6.786 15.158 15.158z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="1.6842" d="M14.868 20.184l4.474-4.474-4.474-4.474"></path>
+        <title>flecha_01</title>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="1.6842" d="M31.158 16c0 8.371-6.786 15.158-15.158 15.158s-15.158-6.786-15.158-15.158c0-8.371 6.786-15.158 15.158-15.158s15.158 6.786 15.158 15.158z"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="1.6842" d="M14.868 20.184l4.474-4.474-4.474-4.474"></path>
       </symbol>
       <symbol id="icon-flecha_02" viewBox="0 0 54 32">
-        <path strokeLinejoin="miter" strokeLinecap="butt" stroke-miterlimit="4" stroke-width="1.6" d="M37.6 0.8c8.395 0 15.2 6.805 15.2 15.2s-6.805 15.2-15.2 15.2c-8.395 0-15.2-6.805-15.2-15.2s6.805-15.2 15.2-15.2z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="1.6" d="M36.8 20l4.25-4.25-4.25-4.25"></path>
-        <path strokeLinejoin="miter" strokeLinecap="round" stroke-miterlimit="4" stroke-width="1.6" d="M41.050 15.75h-39.813"></path>
+        <title>flecha_02</title>
+        <path fill="none" stroke="#0282b0" style={{ stroke: 'var(--color2, #0282b0)' }} stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="1.6" d="M37.6 0.8c8.395 0 15.2 6.805 15.2 15.2s-6.805 15.2-15.2 15.2c-8.395 0-15.2-6.805-15.2-15.2s6.805-15.2 15.2-15.2z"></path>
+        <path fill="none" stroke="#0282b0" style={{ stroke: 'var(--color2, #0282b0)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="1.6" d="M36.8 20l4.25-4.25-4.25-4.25"></path>
+        <path fill="none" stroke="#0282b0" style={{ stroke: 'var(--color2, #0282b0)' }} stroke-linejoin="miter" stroke-linecap="round" stroke-miterlimit="4" stroke-width="1.6" d="M41.050 15.75h-39.813"></path>
       </symbol>
       <symbol id="icon-close" viewBox="0 0 32 32">
-        <path strokeLinejoin="miter" strokeLinecap="square" stroke-miterlimit="4" stroke-width="3.2" d="M30.4 16c0 7.953-6.447 14.4-14.4 14.4s-14.4-6.447-14.4-14.4c0-7.953 6.447-14.4 14.4-14.4s14.4 6.447 14.4 14.4z"></path>
-        <path strokeLinejoin="miter" strokeLinecap="square" stroke-miterlimit="4" stroke-width="3.2" d="M21.236 10.764l-10.473 10.473"></path>
-        <path strokeLinejoin="miter" strokeLinecap="square" stroke-miterlimit="4" stroke-width="3.2" d="M21.236 21.236l-10.473-10.473"></path>
+        <title>close</title>
+        <path fill="#fff" style={{ fill: 'var(--color1, #fff)' }, { stroke: 'var(--color1, #fff)' }} stroke="#fff" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="4" stroke-width="3.2" d="M30.4 16c0 7.953-6.447 14.4-14.4 14.4s-14.4-6.447-14.4-14.4c0-7.953 6.447-14.4 14.4-14.4s14.4 6.447 14.4 14.4z"></path>
+        <path fill="none" stroke="#3f3c4d" style={{ stroke: 'var(--color3, #3f3c4d)' }} stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="4" stroke-width="3.2" d="M21.236 10.764l-10.473 10.473"></path>
+        <path fill="none" stroke="#3f3c4d" style={{ stroke: 'var(--color3, #3f3c4d)' }} stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="4" stroke-width="3.2" d="M21.236 21.236l-10.473-10.473"></path>
       </symbol>
       <symbol id="icon-download_simple" viewBox="0 0 32 32">
-        <path d="M16.333 30c0.7 0 1.167-0.233 1.633-0.7l13.3-13.3-3.267-3.267-9.333 9.333v-20.067h-4.667v20.067l-9.333-9.333-3.267 3.267 13.3 13.3c0.467 0.467 0.933 0.7 1.633 0.7z"></path>
+        <title>download_simple</title>
+        <path fill="#0282b0" style={{ fill: 'var(--color2, #0282b0)' }} d="M16.333 30c0.7 0 1.167-0.233 1.633-0.7l13.3-13.3-3.267-3.267-9.333 9.333v-20.067h-4.667v20.067l-9.333-9.333-3.267 3.267 13.3 13.3c0.467 0.467 0.933 0.7 1.633 0.7z"></path>
       </symbol>
       <symbol id="icon-download" viewBox="0 0 32 32">
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.7826" d="M2.087 22.261v8.348h27.826v-8.348"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.7826" d="M16 1.391v20.87"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.7826" d="M7.652 13.913l8.348 8.348 8.348-8.348"></path>
+        <title>download</title>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.7826" d="M2.087 22.261v8.348h27.826v-8.348"></path>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.7826" d="M16 1.391v20.87"></path>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.7826" d="M7.652 13.913l8.348 8.348 8.348-8.348"></path>
       </symbol>
       <symbol id="icon-filter" viewBox="0 0 32 32">
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M18.667 5.333h12"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M1.333 5.333h4"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M29.333 16h1.333"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M1.333 16h14.667"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M18.667 26.667h12"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M1.333 26.667h4"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M13.333 5.333c0 2.209-1.791 4-4 4s-4-1.791-4-4c0-2.209 1.791-4 4-4s4 1.791 4 4z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M24 16c0 2.209-1.791 4-4 4s-4-1.791-4-4c0-2.209 1.791-4 4-4s4 1.791 4 4z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M13.333 26.667c0 2.209-1.791 4-4 4s-4-1.791-4-4c0-2.209 1.791-4 4-4s4 1.791 4 4z"></path>
+        <title>filter</title>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M18.667 5.333h12"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M1.333 5.333h4"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M29.333 16h1.333"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M1.333 16h14.667"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M18.667 26.667h12"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M1.333 26.667h4"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M13.333 5.333c0 2.209-1.791 4-4 4s-4-1.791-4-4c0-2.209 1.791-4 4-4s4 1.791 4 4z"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M24 16c0 2.209-1.791 4-4 4s-4-1.791-4-4c0-2.209 1.791-4 4-4s4 1.791 4 4z"></path>
+        <path fill="none" stroke="#fff" style={{ stroke: 'var(--color1, #fff)' }} stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" d="M13.333 26.667c0 2.209-1.791 4-4 4s-4-1.791-4-4c0-2.209 1.791-4 4-4s4 1.791 4 4z"></path>
       </symbol>
       <symbol id="icon-info" viewBox="0 0 32 32">
-        <path strokeLinejoin="miter" strokeLinecap="butt" stroke-miterlimit="4" stroke-width="3.5556" d="M30.222 16c0 7.855-6.367 14.222-14.222 14.222s-14.222-6.367-14.222-14.222c0-7.855 6.367-14.222 14.222-14.222s14.222 6.367 14.222 14.222z"></path>
-        <path d="M13.926 13.926h4.148v10.37h-4.148v-10.37z"></path>
-        <path d="M18.074 9.778c0 1.145-0.929 2.074-2.074 2.074s-2.074-0.929-2.074-2.074c0-1.145 0.929-2.074 2.074-2.074s2.074 0.929 2.074 2.074z"></path>
+        <title>info</title>
+        <path fill="#0282b0" style={{ fill: 'var(--color2, #0282b0)' }, { stroke: 'var(--color2, #0282b0)' }} stroke="#0282b0" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="3.5556" d="M30.222 16c0 7.855-6.367 14.222-14.222 14.222s-14.222-6.367-14.222-14.222c0-7.855 6.367-14.222 14.222-14.222s14.222 6.367 14.222 14.222z"></path>
+        <path fill="#fff" style={{ fill: 'var(--color1, #fff)' }} d="M13.926 13.926h4.148v10.37h-4.148v-10.37z"></path>
+        <path fill="#fff" style={{ fill: 'var(--color1, #fff)' }} d="M18.074 9.778c0 1.145-0.929 2.074-2.074 2.074s-2.074-0.929-2.074-2.074c0-1.145 0.929-2.074 2.074-2.074s2.074 0.929 2.074 2.074z"></path>
       </symbol>
       <symbol id="icon-search" viewBox="0 0 32 32">
-        <path strokeLinejoin="miter" strokeLinecap="square" stroke-miterlimit="4" stroke-width="2.7826" d="M29.217 29.217l-8.826-8.826"></path>
-        <path strokeLinejoin="miter" strokeLinecap="square" stroke-miterlimit="4" stroke-width="2.7826" d="M23.652 12.522c0 6.147-4.983 11.13-11.13 11.13s-11.13-4.983-11.13-11.13c0-6.147 4.983-11.13 11.13-11.13s11.13 4.983 11.13 11.13z"></path>
+        <title>search</title>
+        <path fill="none" stroke="#0282b0" style={{ stroke: 'var(--color2, #0282b0)' }} stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="4" stroke-width="2.7826" d="M29.217 29.217l-8.826-8.826"></path>
+        <path fill="none" stroke="#0282b0" style={{ stroke: 'var(--color2, #0282b0)' }} stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="4" stroke-width="2.7826" d="M23.652 12.522c0 6.147-4.983 11.13-11.13 11.13s-11.13-4.983-11.13-11.13c0-6.147 4.983-11.13 11.13-11.13s11.13 4.983 11.13 11.13z"></path>
       </symbol>
       <symbol id="icon-share" viewBox="0 0 32 32">
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M20.060 9.823l-8.119 4.058"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M20.060 22.177l-8.119-4.058"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M29.037 7.704c0 2.618-2.122 4.741-4.741 4.741s-4.741-2.123-4.741-4.741c0-2.618 2.122-4.741 4.741-4.741s4.741 2.123 4.741 4.741z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M29.037 24.296c0 2.618-2.122 4.741-4.741 4.741s-4.741-2.122-4.741-4.741c0-2.618 2.122-4.741 4.741-4.741s4.741 2.122 4.741 4.741z"></path>
-        <path strokeLinejoin="round" strokeLinecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M12.444 16c0 2.618-2.123 4.741-4.741 4.741s-4.741-2.122-4.741-4.741c0-2.618 2.123-4.741 4.741-4.741s4.741 2.123 4.741 4.741z"></path>
+        <title>share</title>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M20.060 9.823l-8.119 4.058"></path>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M20.060 22.177l-8.119-4.058"></path>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M29.037 7.704c0 2.618-2.122 4.741-4.741 4.741s-4.741-2.123-4.741-4.741c0-2.618 2.122-4.741 4.741-4.741s4.741 2.123 4.741 4.741z"></path>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M29.037 24.296c0 2.618-2.122 4.741-4.741 4.741s-4.741-2.122-4.741-4.741c0-2.618 2.122-4.741 4.741-4.741s4.741 2.122 4.741 4.741z"></path>
+        <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="3.5556" d="M12.444 16c0 2.618-2.123 4.741-4.741 4.741s-4.741-2.122-4.741-4.741c0-2.618 2.123-4.741 4.741-4.741s4.741 2.123 4.741 4.741z"></path>
       </symbol>
       <symbol id="icon-zoomin" viewBox="0 0 32 32">
+        <title>zoomin</title>
         <path d="M13.333 0h5.333v32h-5.333v-32z"></path>
         <path d="M0 13.333h32v5.333h-32v-5.333z"></path>
       </symbol>
       <symbol id="icon-zoomout" viewBox="0 0 32 32">
+        <title>zoomout</title>
         <path d="M0 13.333h32v5.333h-32v-5.333z"></path>
       </symbol>
     </defs>
