bc3c2e0da55e500e4cc50b6d6b2d64c738a46e1e
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2020-07-21 13:30:14 +0200

    Footer: use grid

--- src/components/footer/component.js
+++ src/components/footer/component.js
@@ -8,23 +8,25 @@ import './styles.scss';
 
 const Footer = () =>
   <footer className="c-footer">
-    <section className="footer-header">
-      <h2>Do you want to take part?</h2>
-      <div className="footer-controls">
-        <a
-          href={`mailto:?to=post@wetlands.org`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="c-button -border -medium -tertiary"
-        >
-          Contact us
-        </a>
-        <Link className="c-button -background -medium -primary" to={{ type: "REGISTER", payload: { pathname: "register" } }}>Join us</Link>
-      </div>
-    </section>
-    <section className="footer-navigation">
-      <NavMenu className='footer' />
-    </section>
+    <div className="wrapper">
+      <section className="footer-header">
+        <h2>Do you want to take part?</h2>
+        <div className="footer-controls">
+          <a
+            href={`mailto:?to=post@wetlands.org`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="c-button -border -medium -tertiary"
+          >
+            Contact us
+          </a>
+          <Link className="c-button -background -medium -primary" to={{ type: "REGISTER", payload: { pathname: "register" } }}>Join us</Link>
+        </div>
+      </section>
+      <section className="footer-navigation">
+        <NavMenu className='footer' />
+      </section>
+    </div>
     <aside>
       <a
         href="https://www.ead.gov.ae/en"
