ee378b57c279b70f6d209c40e0b09001ded6a8c3
“mluena” <maluenarod@gmail.com>
2020-08-24 12:37:59 +0200

    filters populated from CARTO

---
+++ src/services/publications.js
@@ -0,0 +1,36 @@
+import { setup } from 'axios-cache-adapter';
+import localforage from 'localforage';
+
+const store = localforage.createInstance({
+  driver: [
+    localforage.INDEXEDDB,
+    localforage.LOCALSTORAGE
+  ],
+  name: 'wpe-publications'
+});
+
+export const API = setup({
+  baseURL: process.env.REACT_APP_CARTO_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  cache: {
+    // ignoreCache: process.env.NODE_ENV === 'development',
+    maxAge: 15 * 60 * 1000,
+    exclude: { query: false },
+    store
+  }
+});
+
+export const fetchPublications = () => {
+
+  const q = `SELECT id,description FROM publication WHERE published =1`;
+
+  return API.get(`sql?q=${q}&api_key=${process.env.REACT_APP_CARTO_API_TOKEN}`)
+  .then(({ data }) => data.rows)
+  .catch((e) => {
+    console.log(e, 'error')
+  });
+};
+
+
