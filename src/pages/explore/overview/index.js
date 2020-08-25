217cd52bb2bac8da2b047869eceb471e338e3d72
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2020-08-25 10:00:27 +0200

    Explore: use fami.ies from selector

--- src/pages/explore/overview/index.js
+++ src/pages/explore/overview/index.js
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux';
 import { setPopulations } from 'modules/population/actions';
 
+import { selectPopulationDetailProps } from 'modules/population/selectors';
+
 import Component from './component';
 
-export default connect(null, {
+export default connect(selectPopulationDetailProps, {
   setPopulations
 })(Component);
