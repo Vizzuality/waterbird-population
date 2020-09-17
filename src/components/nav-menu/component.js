79c46523661b5d7ad20dba2e1ecc14b8c0fe807a
“mluena” <maluenarod@gmail.com>
2020-09-17 10:49:29 +0200

    WIP: buger menu

--- src/components/nav-menu/component.js
+++ src/components/nav-menu/component.js
@@ -1,55 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Link from 'redux-first-router-link';
+import Menu from './menu.svg';
 
 import { menuItems } from './constants';
 
 import './styles.scss';
 
 const NavMenu = ({ className, current }) => {
+
+  const [active, setMenu] = useState(false);
+
+  const handleClick = () => {
+    setMenu(!active);
+  }
+
   return (
-    <div className={classnames("c-nav-menu",
-      {
-        'header': className === 'header',
-        'footer': className === 'footer',
-      })}>
-      {menuItems.map(
-        item => (!item.dropdown && !item.component)
-          ? <div key={item.name} className="nav-menu-item">
-            <Link to={{ type: `${item.type}`, pathname: `${item.path}` }}>
-              <h3 className={classnames({ '-active': current === item.type })}>
-                {item.name}
-              </h3>
-            </Link></div>
-          : (item.dropdown && !item.component && (
-            <div key={item.name} className="nav-menu-item dropdown">
-              <Link to={{
-                type: `${item.type}`,
-                pathname: `${item.path}`,
-                payload: { id: `${item.dropdown.options[0].id}` }
-              }}>
-                <h3 className={classnames({ '-active': item.type.includes(current) })}>
+    <div className="c-nav-menu-wrapper">
+      <button
+        className={classnames(
+          {
+            'header': className === 'header',
+            'footer': className === 'footer',
+          })}
+        onClick={handleClick}
+        style={{ backgroundImage: `url(${Menu})` }} />
+      <div className="wrapper">
+      <div className={classnames("nav-menu",
+        {
+          'header': className === 'header',
+          'footer': className === 'footer',
+          '-active': active,
+        })}>
+
+        {menuItems.map(
+          item => (!item.dropdown && !item.component)
+            ? <div key={item.name} className="nav-menu-item">
+              <Link
+                to={{ type: `${item.type}`, pathname: `${item.path}` }}
+                onClick={handleClick}
+              >
+                <h3 className={classnames({ '-active': current === item.type })}>
                   {item.name}
                 </h3>
-              </Link>
-              <ul className="dropdown-list">
-                {item.dropdown.options.map(opt =>
-                  <li key={opt.id}>
-                    <Link to={{
-                      type: `${item.type}`,
-                      pathname: `${item.path}`,
-                      payload: { id: `${opt.id}` }
-                    }}>
-                      {opt.name}
-                    </Link>
-                  </li>
-                )}
-              </ul>
-            </div>
-          )
-        ))
-      }
+              </Link></div>
+            : (item.dropdown && !item.component && (
+              <div key={item.name} className="nav-menu-item dropdown">
+                <Link to={{
+                  type: `${item.type}`,
+                  pathname: `${item.path}`,
+                  payload: { id: `${item.dropdown.options[0].id}` }
+                }}
+                  onClick={handleClick}
+                >
+                  <h3 className={classnames({ '-active': item.type.includes(current) })}>
+                    {item.name}
+                  </h3>
+                </Link>
+                <ul className="dropdown-list">
+                  {item.dropdown.options.map(opt =>
+                    <li key={opt.id}>
+                      <Link to={{
+                        type: `${item.type}`,
+                        pathname: `${item.path}`,
+                        payload: { id: `${opt.id}` }
+                      }}
+                        onClick={handleClick}
+                      >
+                        {opt.name}
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              </div>
+            )
+            ))
+        }
+      </div>
+      </div>
     </div>
   )
 };
