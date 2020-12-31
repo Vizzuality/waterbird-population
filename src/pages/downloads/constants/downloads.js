f6a91a23aacb1cd987474692e09225a81a95eacd
mluena <maluenarod@gmail.com>
2020-12-31 08:29:00 +0100

    references and download links modified

--- src/pages/downloads/constants/downloads.js
+++ src/pages/downloads/constants/downloads.js
@@ -8,28 +8,59 @@ const Downloads = [
         <br />
         <div className="row">
           <div className="col-sm-9">
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/CSR7%20Methodology%20Notes.pdf">
-              AEWA Conservation Status Report Methodology Notes
-            </a>
+            <p>
+              AEWA Conservation Status Report Methodology Notes:
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/CSR7%20Methodology%20Notes.pdf">
+                [PDF]
+              </a>
+            </p>
           </div>
         </div>
 
         <div className="row">
           <div className="col-sm-9">
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/wpe5.pdf">
-              Waterbird Population Estimates 5 (2012) (pdf)
-            </a>
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/wpe5.xls">
-              / (xls)
-            </a>
+            <p>
+              Waterbird Population Estimates 5 (2012):
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/wpe5.pdf">
+                [PDF]
+              </a>
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/wpe5.xls">
+                [XLS]
+              </a>
+            </p>
           </div>
         </div>
 
         <div className="row">
           <div className="col-sm-9">
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/wpe4.xls">
-              Waterbird Population Estimates 4 (2007)
-            </a>
+            <p>Waterbird Population Estimates 4 (2007):
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/wpe4.xls">
+                [PDF]
+              </a>
+            </p>
+
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-9">
+            <p>Waterbird Population Estimates 3 (2002)</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-9">
+            <p>Waterbird Population Estimates 2 (1997)</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-9">
+            <p>Waterbird Population Estimates 1 (1994)</p>
           </div>
         </div>
 
@@ -42,17 +73,35 @@ const Downloads = [
 
         <div className="row">
           <div className="col-sm-9">
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/major_flyways.kml">Major Flyways</a>
+            <p>
+              Major Flyways:
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/major_flyways.kml">
+                [KML]
+              </a>
+            </p>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-9">
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/ramsar_regions.kml">Ramsar Regions</a>
+            <p>
+              Ramsar Regions:
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/ramsar_regions.kml">
+                [KML]
+              </a>
+            </p>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-9">
-            <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/biogeographic_regions.kml">Biogeographic Regions</a>
+            <p>
+              Biogeographic Regions:
+              {' '}
+              <a href="http://wpe.wetlands.org/bundles/voidwalkerswpe/images/biogeographic_regions.kml">
+                [KML]
+              </a>
+            </p>
           </div>
         </div>
       </div>
@@ -61,3 +110,5 @@ const Downloads = [
 ];
 
 export default Downloads;
+
+
