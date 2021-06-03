9a307080a7af55b22afbc818de89eb8ee194d656
mluena <maluenarod@gmail.com>
2021-06-03 08:16:09 +0200

    wrapper component for signin and sign up

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'redux-first-router-link';
 import Logo from 'images/logo.png';
 import NavMenu from 'components/nav-menu';
-import Login from 'components/login';
+import SigInSigupContainer from 'components/user-signin';
 
 import './styles.scss';
 
@@ -11,14 +11,14 @@ const Header = () => (
     <div className="wrapper">
       <div className="row">
         <div className="col-xs-3">
-          <Link to={{ type: "HOME", pathname: "/" }}>
+          <Link to={{ type: 'HOME', pathname: '/' }}>
             <img src={Logo} alt="Wetlands International" />
           </Link>
         </div>
         <div className="col-xs-9">
           <div className="header-navigation">
-            <NavMenu className='header' />
-            <Login />
+            <NavMenu className="header" />
+            <SigInSigupContainer modalContent="sign-in" />
           </div>
         </div>
       </div>
